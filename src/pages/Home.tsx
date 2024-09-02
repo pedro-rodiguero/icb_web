aaaf6a3eb4e807c6f1bf5cbb46662903ca6c4d9e
pemiinem <190139323@aluno.unb.br>
2024-09-02 11:25:48 -0300

    chore: update eslint configuration and dependencies

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,28 @@
-import React from 'react';
+import React from "react";
+import { Link } from "react-router-dom";
+
+import ImageCarousel from "../components/ImageCarousel";
+import YtHome from "../images/yt_home.svg";
+import "../styles/Home.css";
 
 const Home: React.FC = () => {
-  return <div>Home Page</div>;
+  const imageUrls = [
+    "https://storage.cloud.google.com/icb-web-bucket/photo1.JPG",
+    "https://storage.cloud.google.com/icb-web-bucket/photo2.JPG",
+  ];
+  return (
+    <div>
+      <ImageCarousel imageUrls={imageUrls} />
+      <Link
+        to="https://www.youtube.com/c/IgrejadeCristoemBras%C3%ADlia/featured"
+        className="yt_home"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <img src={YtHome} alt="Youtube" />
+      </Link>
+    </div>
+  );
 };
 
 export default Home;
